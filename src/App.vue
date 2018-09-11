213aa7dbd928f8f9ec1dc0ce9a2149e9bbeb2b7a
PTamako <ptamako@qq.com>
2018-09-11 17:40:13 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,31 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link :to="{path:'/goods'}">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{path:'/ratings'}">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{path:'/seller'}">商家</router-link>
+      </div>
     </div>
-    <div class="content">I am content</div>
-
+    <div class="content">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
   </div>
 </template>
 
 <script>
-  import vheader from './components/header/vheader'
+  import VHeader from './components/header/vheader'
 
   export default {
     name: 'App',
     components: {
-      VHeader: vheader
+      VHeader
     }
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from './../App'
+import Index from './../components/content/content'
+import Goods from './../components/goods/goods'
+import Ratings from './../components/ratings/ratings'
+import Seller from './../components/seller/seller'
 
 Vue.use(Router)
 
@@ -11,6 +14,21 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
-    }
+    },
+    {
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
+    },
   ]
 })
