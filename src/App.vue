c1cda6e60196e28f56713125447827f3c22e0d60
PTamako <ptamako@qq.com>
2018-09-12 10:43:02 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,7 @@
         width: 100%
         height: 40px
         line-height: 40px
+        border-bottom 1px solid rgba(7, 17, 27, .1)
         li
           flex: 1
           text-align: center
