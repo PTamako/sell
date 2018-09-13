edfda9e1ed90642dd6a637aa618ef4ba902c700f
PTamako <ptamako@qq.com>
2018-09-13 16:35:15 +0800

    ok

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,11 +1,11 @@
 <template>
-    
+  <div>I am ratings</div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: "ratings"
-    }
+  export default {
+    name: "ratings"
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
