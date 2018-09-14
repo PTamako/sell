428f6b5a12bf2452c44ddcf74582a5f22fea6f29
PTamako <ptamako@qq.com>
2018-09-14 14:33:04 +0800

    ok

--- src/components/header/vheader.vue
+++ src/components/header/vheader.vue
@@ -35,17 +35,43 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./../../common/stylus/mixin.styl"
   .header
     color: #fff
     background #7e8c8d
     .content-wrapper
-      padding: 24px 12px 18px 24px
+      padding 24px 12px 18px 24px
       font-size 0
       .avatar
-        display: inline-block
+        display inline-block
         font-size 14px
+        vertical-align top
+        img
+          border-radius 3px
       .content
-        display: inline-block
+        display inline-block
         margin-left 16px
         font-size 14px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            display inline-block
+            vertical-align top
+            width 30px
+            height 18px
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 6px
+            font-size 16px
+            line-height 18px
+            font-weight bold
+        .description
+          margin-bottom 10px
+          font-size 12px
+          line-height 12px
+        .support
+          font-size 12px
+          line-height 12px
 </style>
