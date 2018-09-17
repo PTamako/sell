79f7ac7c8ddde31ec7498d95dd25d5c592ee5327
PTamako <ptamako@qq.com>
2018-09-17 11:05:58 +0800

    ok

--- src/components/header/vheader.vue
+++ src/components/header/vheader.vue
@@ -17,17 +17,27 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count" @click="">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bullentin-wrapper">
+    <div class="bullentin-wrapper" @click="showDetail">
       <span class="bullentin-title"></span><span class="bullentin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close" @click="closeDetail">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +45,19 @@
 <script type="text/ecmascript-6">
   export default {
     name: "header",
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
+      },
+      closeDetail() {
+        this.detailShow = false;
+      }
+    },
     props: {
       seller: {
         type: Object,
@@ -161,4 +184,32 @@
       height 100%
       z-index -1
       filter: blur(10px)
+    .detail
+      position fixed
+      top 0
+      left 0
+      z-index 100
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, .8)
+      .detail-wrapper
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height: 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+      .detail-close
+        position relative
+        width: 32px
+        height: 32px
+        margin -64px auto 0 auto
+        clear both
+        font-size 32px
+
 </style>
