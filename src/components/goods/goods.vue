db079cab7899254137fdcb9817a4b00faec6e22b
PTamako <ptamako@qq.com>
2018-09-19 16:00:24 +0800

    ok

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  const ERR_OK = 0;
   export default {
-    name: "goods"
+    name: "goods",
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    created() {
+      this.$http.get('/api/goods')
+        .then((res) => {
+          res = res.body;
+          if (res.errno === ERR_OK) {
+            this.goods = res.data;
+            console.log(this.goods);
+          }
+        })
+    },
+    data() {
+      return {
+        goods: []
+      }
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display flex
+    overflow hidden
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+    .foods-wrapper
+      flex 1
 </style>
