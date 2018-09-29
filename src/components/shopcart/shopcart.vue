7696008fa5b5cd5804c365db80bb73b9d03a4777
PTamako <ptamako@qq.com>
2018-09-29 11:41:09 +0800

    ok

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shopcart">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight':totalCount > 0}">
@@ -29,25 +29,27 @@
         </transition>
       </div>
     </div>
-    <div class="shopcart-list" v-show="listShow">
-      <div class="list-header">
-        <h1 class="title">购物车</h1>
-        <span class="empty">清空</span>
+    <transition name="fold">
+      <div class="shopcart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty">清空</span>
+        </div>
+        <div class="list-content">
+          <ul>
+            <li v-for="food in selectFoods" class="food">
+              <span class="name">{{food.name}}</span>
+              <div class="price">
+                <span>¥ {{food.price*food.count}}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <cartcontrol :food="food"></cartcontrol>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="list-content">
-        <ul>
-          <li v-for="food in selectFoods" class="food">
-            <sapn class="name">{{food.name}}</sapn>
-            <div class="price">
-              <span>¥ {{food.price*food.count}}</span>
-            </div>
-            <div class="cartcontrol-wrapper">
-              <cartcontrol :food="food"></cartcontrol>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -99,10 +101,19 @@
             show: false
           }
         ],
-        dropBalls: []
+        dropBalls: [],
+        fold: true
       }
     },
     computed: {
+      listShow() {
+        if (!this.totalCount) {
+          this.fold = true;
+          return false;
+        }
+        let show = !this.fold;
+        return show;
+      },
       totalPrice() {
         let total = 0;
         this.selectFoods.forEach((food) => {
@@ -134,6 +145,13 @@
       }
     },
     methods: {
+      toggleList() {
+        if (!this.totalCount) {
+          return;
+        } else {
+          this.fold = !this.fold
+        }
+      },
       drop(el) {
         for (let i = 0; i < this.balls.length; i++) {
           let ball = this.balls[i];
@@ -185,6 +203,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import './../../common/stylus/mixin.styl'
   .shopcart
     position fixed
     left 0
@@ -286,4 +305,40 @@
           border-radius 50%
           background-color rgb(0, 160, 220)
           transition all 0.4s linear
+    .shopcart-list
+      position absolute
+      top 0
+      left 0
+      z-index -1
+      width 100%
+      transform translate3d(0, -100%, 0)
+      &.fold-enter-active, &.fold-leave-active
+        transition all 0.5s
+      &.fold-enter, &.fold-leave-active
+        opacity 0
+        transform translate3d(0, 0, 0)
+      .list-header
+        height: 40px
+        line-height: 40px
+        padding 0 18px
+        background #f3f3f7
+        border-bottom 1px solid rgba(7, 17, 27, .1)
+        .title
+          float left
+          font-size 14px
+          color rgb(7, 17, 27)
+        .empty
+          float right
+          font-size 12px
+          color: rgb(0, 160, 220)
+      .list-content
+        padding: 0 18px
+        max-height 217px
+        overflow hidden
+        background: #fff
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing border-box
+          border-1px(rgba(7, 17, 27, .1))
 </style>
